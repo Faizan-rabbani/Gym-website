49dcc2b2b087ed7a4ab34b0d31adb3f16adc8a75
Faizan Rabbani <faizanrabbani987@gmail.com>
2025-07-20 18:25:45 +0500

    updated version

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ document.getElementById("gymForm").addEventListener("submit", function (e) {
       this.style.display = "none";
       document.getElementById("successMessage").style.display = "block";
     });
+
+function toggleMenu() {
+  document.querySelector(".navbar").classList.toggle("open");
+}
